bf2d1b5b5d889a5075cc29d439d21eb17da84ff7
HeyArtem <artem_white@mail.ru>
2020-05-23 01:06:47 +0300

    3.7 ДЗ после 3гл. Операции со словарями, строками, списками, лемматизация

--- 3.7_ДЗ_3_глава.py
+++ 3.7_ДЗ_3_глава.py
@@ -3,9 +3,10 @@
 import string
 f = open('123.txt','r', encoding='UTF8')
 text=f.read()
-print(type(text))
+#print(type(text), text)
 
 # 1. Методами строк очистить текст от знаков препинания;
+#print('1 Методами строк очистить текст от знаков препинания')
 
 my_str = text.replace('.', ' ')
 my_str = my_str.replace(',', ' ')
@@ -20,52 +21,59 @@ my_str = my_str.replace(')', ' ')
 my_str = my_str.replace('(', ' ')
 my_str = my_str.replace(':', ' ')
 
-print(type(my_str), my_str)
+#print(type(my_str), my_str)
 
 
-# 2.Cформировать list со словами (split)
+#2.Cформировать list со словами (split)
+#print(' 2.Cформировать list со словами (split) ')
 my_str_list = my_str.split()
-print(type(my_str_list), my_str_list)
+#print(type(my_str_list), my_str_list)
+
+
 
 # 3.привести все слова к нижнему регистру (map);
+#print(' 3.привести все слова к нижнему регистру (map) ')
 new_list = list(map(lambda x: x.lower(), my_str_list))
-print(type(new_list), new_list)
+#print(type(new_list), new_list)
+
 
 # 3.(опять п.3) Получить из list пункта 3 dict, ключами которого являются слова, а значениями их количество появлений в тексте;
-from collections import Counter
-new_list2 = Counter(new_list)
-print(type(new_list2),new_list2)
+#print(' 3.(опять п.3) Получить из list пункта 3 dict, ключами которого являются слова, а значениями их количество появлений в тексте; ')
+
+my_dict = {a:new_list.count(a) for a in new_list} # это не моё реш. почему a,  (i не льзя)? какая логика
+#print(my_dict)
+
+
+
+
 
 
 # 4. Bывести 5 наиболее часто встречающихся слов (sort), вывести количество разных слов в тексте (set).
+#print(' 4. Bывести 5 наиболее часто встречающихся слов (sort), вывести количество разных слов в тексте (set)')
+
+pop_words = []
+for a in my_dict.values():
+    pop_words.append(a)
+pop_words.sort()
+
+pop5words = pop_words[-5:]
+for keys, values in my_dict.items():
+    if values in pop5words:
+#        print(keys, values)
+
+total_words = len(set(my_dict))
+#print('Всего в тексте разновидностей слов: ', total_words)
 
 
 
-#with open('123.txt', 'r') as file:
-#   responce = file.read().replace('\n', ").replace('\r',")
+# 5. Выполнить light с условием: в пункте 2 дополнительно к приведению к нижнему регистру выполнить лемматизацию.
+#print(' 5. Выполнить light с условием: в пункте 2 дополнительно к приведению к нижнему регистру выполнить лемматизацию. ')
+import pymorphy2
+morph = pymorphy2.MorphAnalyzer()
 
 
+my_lemma = []
+for a in new_list:
+    my_lemma.append(morph.parse(a)[0].normal_form)
+#print(my_lemma)
 
-
-
-
-#print(type(my_str))
-
-
-
-
-
-
-
-
-
-#responce=file.read().replace('\n',").replace('\r',")
-
-#print(type(my_list))
-# Методами строк очистить текст от знаков препинания.
-#my_str2 = my_str("{" [.] [" "] [":"]"}")
-# my_str2 = my_str.replace('.', 'ЗАМЕНИЛ ТОЧКУ')
-#my_str.find('заб', [0], [200])
-
-#my_str2 = my_str.replace('.', 'ЗАМЕНИЛ ТОЧКУ')
-#print(my_str2)
